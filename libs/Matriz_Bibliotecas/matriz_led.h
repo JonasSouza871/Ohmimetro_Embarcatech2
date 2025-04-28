058cdbee8aedc413fd87998f9423274ad23847f5
Jonas Souza <jonasssouza871@hotmai.com>
2025-04-28 11:47:45 -0300

    Corrigindo Matriz_led.h

--- libs/Matriz_Bibliotecas/matriz_led.h
+++ libs/Matriz_Bibliotecas/matriz_led.h
@@ -13,19 +13,9 @@
 #define NUM_PIXELS    (NUM_LINHAS * NUM_COLUNAS) // Total 25
 #define RGBW_ATIVO    false // Se os LEDs são RGBW ou RGB
 
-// Função para inicializar a PIO para controlar os LEDs WS2812
 void inicializar_matriz_led();
-
-// Função interna para enviar um único pixel (não precisa ser exposta no .h geralmente)
-// void enviar_pixel(uint32_t pixel_grb);
-
-// --- NOVA FUNÇÃO ---
-// Mostra as 3 cores das faixas nas linhas 1, 3 e 5 da matriz
-// As outras linhas ficam apagadas.
 // Parâmetros: Ponteiros para os nomes das cores (ex: "Vermelho")
 void mostrar_faixas_cores(const char *cor_faixa1, const char *cor_faixa2, const char *cor_faixa3);
-
 // Função para desligar todos os LEDs da matriz
 void desligar_matriz();
-
 #endif // MATRIZ_LED_H
