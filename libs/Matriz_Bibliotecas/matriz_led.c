3ddf90d72bb18375f6487ec93915456d3cab4466
Jonas Souza <jonasssouza871@hotmai.com>
2025-04-28 11:40:16 -0300

    Alterando colocaracao matriz led

--- libs/Matriz_Bibliotecas/matriz_led.c
+++ libs/Matriz_Bibliotecas/matriz_led.c
@@ -14,12 +14,12 @@ const CorRGB PALETA_CORES[] = {
     {"Preto", 0, 0, 0}, //cor preta não tem valor rgb
     {"Marrom",   30,   10,    10},  
     {"Vermelho", 190,   0,    0},  
-    {"Laranja",  255,  90,    0},  
-    {"Amarelo",  255, 180,    0},  
+    {"Laranja",  255,  65,    0},  
+    {"Amarelo",  255, 140,    0},  
     {"Verde",     0,  150,    0},
     {"Azul",      0,    0,  200}, 
     {"Violeta", 130,    0,  130},  
-    {"Cinza",    80,   80,   80},
+    {"Cinza",   40,    35,  35},
     {"Branco",  255,  255,  255},  // Branco verdadeiro
     {"Prata",   192,  192,  192},
     {"Ouro",    218,  165,   32},
